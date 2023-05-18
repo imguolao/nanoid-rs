00d6dfec87642c02b7b8d82ef9be90fd75a8cbaa
imguolao <guolao.mq@foxmail.com>
2023-05-18 15:58:25 +0800

    feat: nanoid macro

--- src/main.rs
+++ src/main.rs
@@ -7,19 +7,40 @@ use rand::{thread_rng, Rng};
 // References to the brotli default dictionary:
 // `-26T`, `1983`, `40px`, `75px`, `bush`, `jack`, `mind`, `very`, and `wolf`
 const URL_ALPHABET: &str = "useandom-26T198340PX75pxJACKVERYMINDBUSHWOLF_GQZbfghjklqvwyzrict";
-const ALPHABET_SIZE: usize = URL_ALPHABET.len();
 
-fn nanoid(size: usize) -> String {
-    let mut id = String::with_capacity(size);
-    let mut rng = thread_rng();
-    for _ in 0..size {
-        let index = rng.gen_range(0..ALPHABET_SIZE);
-        id.push(URL_ALPHABET.chars().nth(index).unwrap());
+fn format(size: usize, alphabet: &str) -> String {
+    let len = alphabet.len();
+    if len == 0 {
+        panic!("Can not use a empty alphabet to generate a nanoid.");
     }
+
+    let mut rng = thread_rng();
+    let id: String = (0..size)
+        .map(|_| rng.gen_range(0..len))
+        .map(|i| alphabet.chars().nth(i).unwrap())
+        .collect();
 
     id
 }
 
+macro_rules! nanoid {
+    // default
+    () => {
+        format(21, URL_ALPHABET)
+    };
+
+    ($size:expr) => {
+        format($size, URL_ALPHABET)
+    };
+
+    ($size:expr, $alphabet:expr) => {
+        format($size, $alphabet)
+    };
+}
+
 fn main() {
-    println!("{}", nanoid(21));
+    println!("{}", nanoid!());
+    println!("{}", nanoid!(21));
+    println!("{}", nanoid!(21, "0123456789_-"));
+    // println!("{}", nanoid!(21, ""));
 }
